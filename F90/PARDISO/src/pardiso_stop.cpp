2ea2dddc0a90aa2d55819776cf5a800b1f57bade
Santiago Ontanon <sontanon@gmail.com>
2018-05-04 00:41:15 -0500

    Add F90 PARDISO version.

---
+++ F90/PARDISO/src/pardiso_stop.cpp
@@ -0,0 +1,27 @@
+// PARDISO parameters and prototypes.
+#include "pardiso_param.h"
+#include "pardiso.h"
+
+// Clear PARDISO memory and structures.
+extern "C" void pardiso_stop_(void)
+{
+#ifdef VERBOSE
+	printf("PARDISO: Clearing internal memory...\n");
+#endif
+	// Termination and release of memory.
+	phase = -1;
+	pardiso(pt, &maxfct, &mnum, &mtype, &phase, 
+		&n, &ddum, &idum, &idum, &idum, &nrhs,  
+		iparm, &msglvl, &ddum, &ddum, &error, dparm);
+#ifdef VERBOSE
+	printf("PARDISO: Internal memory clear.\n");
+#endif
+
+	// Delete permutation vector.
+	free(perm);
+#ifdef VERBOSE
+	printf("PARDISO: All memory clear.\n");
+#endif
+
+	return;
+}
